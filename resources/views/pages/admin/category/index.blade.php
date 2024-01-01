d12d40f6b817ecdfd7106031f857951547cf12e0
cindytyas <cindysyaira21@gmail.com>
2024-01-02 00:23:21 +0700

    Initial commit

---
+++ resources/views/pages/admin/category/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.admin')
+
+@section('title')
+    Category
+@endsection
+
+@section('content')
+    <div class="section-content section-dashboard-home" data-aos="fade-up">
+        <div class="container-fluid">
+            <div class="dashboard-heading">
+                <h2 class="dashboard-title">Category</h2>
+                <p class="dashboard-subtitle">
+                    List of Categories
+            </div>
+            <div class="dashboard-content">
+                <div class="row">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,52 @@
+<?php
+
+use App\Http\Controllers\Admin\CategoryController as AdminCategoryController;
+use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DashboardProductController;
+use App\Http\Controllers\DashboardSettingController;
+use App\Http\Controllers\DashboardTransactionController;
+use App\Http\Controllers\DetailController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/categories', [CategoryController::class, 'index'])->name('categories');
+Route::get('/details/{id}', [DetailController::class, 'index'])->name('detail');
+Route::get('/cart', [CartController::class, 'index'])->name('cart');
+Route::get('/success', [CartController::class, 'success'])->name('success');
+
+Route::get('/register/success', [RegisterController::class, 'success'])->name('register-success');
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/dashboard/product', [DashboardProductController::class, 'index'])->name('dashboard-product');
+Route::get('/dashboard/products/create', [DashboardProductController::class, 'create'])->name('dashboard-product-create');
+Route::get('/dashboard/products/{id}', [DashboardProductController::class, 'details'])->name('dashboard-product-details');
+Route::get('/dashboard/transactions', [DashboardTransactionController::class, 'index'])->name('dashboard-transaction');
+Route::get('/dashboard/transactions/{id}', [DashboardTransactionController::class, 'details'])->name('dashboard-transaction-details');
+
+Route::get('/dashboard/settings', [DashboardSettingController::class, 'store'])->name('dashboard-settings-store');
+Route::get('/dashboard/account', [DashboardSettingController::class, 'account'])->name('dashboard-settings-account');
+
+Route::prefix('admin')->namespace('Admin')
+    ->group(function() {
+        Route::get('/', [AdminDashboardController::class, 'index'])->name('admin-dashboard');
+        Route::resource('Category', [AdminCategoryController::class]);
+    });
+    // ->middleware(['auth','admin'])
+Auth::routes();
+
